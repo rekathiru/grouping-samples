49246a7545ef347e6467a06a11fb16584d0fbfd9
Lahiru Sandaruwan <lahirus@apache.org>
2014-12-15 01:53:48 +0530

    update dependency scaling artifacts

--- dependency_scaling/mock/deploy.sh
+++ dependency_scaling/mock/deploy.sh
@@ -4,7 +4,7 @@
 curl -X POST -H "Content-Type: application/json" -d @'json/autoscale-policy.json' -k -v -u admin:admin https://localhost:9443/api/autoscalingPolicies
 
 curl -X POST -H "Content-Type: application/json" -d @'json/tomcat.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
-curl -X POST -H "Content-Type: application/json" -d @'json/tomcat1.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
+curl -X POST -H "Content-Type: application/json" -d @'json/php.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
 
 sleep 3
 
