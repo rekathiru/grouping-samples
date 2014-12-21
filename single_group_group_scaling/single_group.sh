12076f69eec8c19c2b3de2face14d1c721aa4900
Lahiru Sandaruwan <lahirus@apache.org>
2014-12-21 20:02:35 +0530

    Update group info

--- single_group_group_scaling/single_group.sh
+++ single_group_group_scaling/single_group.sh
@@ -4,8 +4,7 @@
 curl -X POST -H "Content-Type: application/json" -d @'grouping/autoscale-policy.json' -k -v -u admin:admin https://localhost:9443/api/autoscalingPolicies
 
 curl -X POST -H "Content-Type: application/json" -d @'grouping/tomcat.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
-curl -X POST -H "Content-Type: application/json" -d @'grouping/tomcat1.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
-curl -X POST -H "Content-Type: application/json" -d @'grouping/tomcat2.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
+
 curl -X POST -H "Content-Type: application/json" -d @'grouping/group6c.json' -k -v -u admin:admin https://localhost:9443/api/groups
 
 sleep 3
@@ -15,5 +14,4 @@ curl -X POST -H "Content-Type: application/json" -d @'grouping/app_single_group.
 sleep 3
 curl -X POST -H "Content-Type: application/json" -d@'grouping/dep_single_group.json' -k -v -u admin:admin https://localhost:9443/api/application/app_group_v1/deploy
 
-#curl -X POST -H "Content-Type: application/json" -d@'grouping/dep_single_group.json' -k -v -u admin:admin https://localhost:9443/api/applicationDeployments
 
