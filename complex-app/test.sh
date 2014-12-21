3e1bfa39144ddde3d75dd3400427ce3d73f5919c
reka <rthirunavukkarasu23@gmail.com>
2014-12-21 21:24:57 +0530

    updating the configs

--- complex-app/test.sh
+++ complex-app/test.sh
@@ -30,7 +30,6 @@ sleep 5
 # curl -X GET -H "Content-Type: application/json" -k -v -u admin:admin https://localhost:9443/api/applications/myapp1265
 
 # Deploy application
-curl -X POST -H "Content-Type: application/json" -d@'deployment-policy.json' -k -v -u admin:admin https://localhost:9443/api/applicationDeployments
 
 sleep 3
 curl -X POST -H "Content-Type: application/json" -d@'grouping/dep_single_group.json' -k -v -u admin:admin https://localhost:9443/api/application/app_boo/deploy
