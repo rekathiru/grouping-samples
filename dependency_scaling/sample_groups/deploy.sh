f4ed64edcd17655275658f893f82a5c79444514e
Lahiru Sandaruwan <lahirus@apache.org>
2015-01-05 09:42:46 +0530

    update dependency scaling with groups

--- dependency_scaling/sample_groups/deploy.sh
+++ dependency_scaling/sample_groups/deploy.sh
@@ -4,8 +4,8 @@
 curl -X POST -H "Content-Type: application/json" -d @'json/autoscale-policy.json' -k -v -u admin:admin https://localhost:9443/api/autoscalingPolicies
 
 curl -X POST -H "Content-Type: application/json" -d @'json/tomcat.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
-curl -X POST -H "Content-Type: application/json" -d @'json/tomcat1.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
-curl -X POST -H "Content-Type: application/json" -d @'json/tomcat2.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
+curl -X POST -H "Content-Type: application/json" -d @'json/esb.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
+curl -X POST -H "Content-Type: application/json" -d @'json/php.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
 curl -X POST -H "Content-Type: application/json" -d @'json/group6c.json' -k -v -u admin:admin https://localhost:9443/api/groups
 
 sleep 3
@@ -13,5 +13,6 @@ sleep 3
 curl -X POST -H "Content-Type: application/json" -d @'json/app_single_group.json' -k -v -u admin:admin https://localhost:9443/api/applications
 
 sleep 3
-curl -X POST -H "Content-Type: application/json" -d@'json/dep_single_group.json' -k -v -u admin:admin https://localhost:9443/api/applicationDeployments
+
+curl -X POST -H "Content-Type: application/json" -d@'json/dep_single_group.json' -k -v -u admin:admin https://localhost:9443/api/applications/app_group_v2/deploy
 
